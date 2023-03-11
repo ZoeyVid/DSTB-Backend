8426b7933a95d68406439147b4400b2c1e6e31ee
David <david@davidcraft.de>
2023-03-11 15:25:19 +0000

    DB Test 1
    Signed-off-by: GitHub <noreply@github.com>

--- database.js
+++ database.js
@@ -1,7 +1,23 @@
 const mysql = require('mysql');
+const config = require('./config.json');
+let connection
 
-module.exports = function() {
-    console.log("Database module loaded!")
+module.exports = {
+    connect: function() {
+        connection = mysql.createConnection({
+            host: config.host,
+            user: config.user,
+            password: config.password,
+            database: config.database
+        });
+        connection.connect(function(err) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            }
+            console.log("Es geht");
+        });
+    }
 }
 
 // https://www.w3schools.com/nodejs/nodejs_mysql.asp
