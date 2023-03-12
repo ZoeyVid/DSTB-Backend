aa2157bd902bbffa1bece6f5dd7f06337bf0e641
David <david@davidcraft.de>
2023-03-12 15:49:09 +0000

    Add setup
    Signed-off-by: GitHub <noreply@github.com>

---
+++ setup.js
@@ -0,0 +1,8 @@
+const database = require("./database.js");
+
+console.log("Create tables...");
+
+database.connect();
+database.query("CREATE TABLE IF NOT EXISTS `users` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL, `password` VARCHAR(255) NOT NULL, `email` VARCHAR(255) NOT NULL, `co` VARCHAR(255) NOT NULL, `role` VARCHAR(255) NOT NULL, `parent` VARCHAR(255) NOT NULL, `teacher` VARCHAR(255) NOT NULL, `admin` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));");
+var test = database.query("SELECT count(*) FROM information_schema.TABLES WHERE TABLE_NAME = 'users'");
+console.log(test);
