d9564a3b54df39a669858e15503614f56d72402a
David <david@davidcraft.de>
2023-03-13 15:51:41 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -6,5 +6,5 @@ createTables();
 
 async function createTables() {
 await database.connect();
-console.log(await database.query("CREATE TABLE test (name VARCHAR(255), age INT)"));
+console.log(await database.query("CREATE TABLE IF NOT EXIST dstb_user (id INT(11) NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, deactivateted boolean NOT NULL DEFAULT 0, PRIMARY KEY (id))"));
 }
