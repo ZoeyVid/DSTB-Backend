2bde200bfbb0de0256610021f5b5669c58c87af7
David <david@davidcraft.de>
2023-03-13 15:31:36 +0000

    So
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -2,8 +2,12 @@ const database = require("./database.js");
 
 console.log("Create tables...");
 
-database.connect();
-database.query("CREATE TABLE IF NOT EXISTS `users` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL, `password` VARCHAR(255) NOT NULL, `email` VARCHAR(255) NOT NULL, `co` VARCHAR(255) NOT NULL, `role` VARCHAR(255) NOT NULL, `parent` VARCHAR(255) NOT NULL, `teacher` VARCHAR(255) NOT NULL, `admin` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));");
-var test = database.query("SELECT count(*) FROM information_schema.TABLES WHERE TABLE_NAME = 'users'");
+createTables();
+
+async function createTables() {
+await database.connect();
+await database.query("CREATE TABLE IF NOT EXISTS `users` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL, `password` VARCHAR(255) NOT NULL, `email` VARCHAR(255) NOT NULL, `co` VARCHAR(255) NOT NULL, `role` VARCHAR(255) NOT NULL, `parent` VARCHAR(255) NOT NULL, `teacher` VARCHAR(255) NOT NULL, `admin` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));");
+var test = await database.query("SELECT count(*) FROM information_schema.TABLES WHERE TABLE_NAME = 'users'");
 console.log(test);
-database.disconnect();
+await database.disconnect();
+}
