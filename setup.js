a23b9467e9a6b4eb8c50dd83f2b1067b5925a2ba
David <david@davidcraft.de>
2023-03-13 15:43:35 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -6,8 +6,5 @@ createTables();
 
 async function createTables() {
 await database.connect();
-console.log(await database.query("CREATE TABLE IF NOT EXISTS `users` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL, `password` VARCHAR(255) NOT NULL, `email` VARCHAR(255) NOT NULL, `co` VARCHAR(255) NOT NULL, `role` VARCHAR(255) NOT NULL, `parent` VARCHAR(255) NOT NULL, `teacher` VARCHAR(255) NOT NULL, `admin` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`));"))
-var test = await database.query("SELECT count(*) FROM information_schema.TABLES WHERE TABLE_NAME = 'users'");
-console.log(test);
-await database.disconnect();
+console.log(await database.query("SELECT * FROM information_schema.TABLES"));
 }
