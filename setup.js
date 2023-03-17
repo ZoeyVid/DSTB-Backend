e99066e40fc9f4929d79895b9cc34038fa798226
David <david@davidcraft.de>
2023-03-17 15:47:57 +0000

    So
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -9,8 +9,8 @@ async function createTables() {
 await database.connect(config);
 await database.query("DROP TABLE IF EXISTS dstb_user");
   // UserID, UserName, FirstName, LastName, Password-Hash, Account-Type
-console.log(await database.query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))"));
-database.query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('test', 'test', 'test', 'test', 'test')");
+await database.query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
+database.query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('" + config.credentials.username + "', 'Admin', 'Admin', 'test', 'Admin')");
 var test = await database.query("SELECT * FROM dstb_user");
 console.log(test);
 }
