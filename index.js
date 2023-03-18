dfaa30cb8e73bbb706bb7b29e47c797b7fc107c2
David <david@davidcraft.de>
2023-03-18 15:38:08 +0000

    So
    Signed-off-by: GitHub <noreply@github.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Hello World!");
 
 console.log("Load express module...");
-require("./express.js")(config);
+require("./express.js")();
 
 console.log("Load database module...");
 const { connect } = require("./database.js");
