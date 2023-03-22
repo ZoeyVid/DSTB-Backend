a8dbfd6fef90dceeff3edc4f3ad8b471937adfbf
David <david@davidcraft.de>
2023-03-22 15:45:55 +0000

    ah
    Signed-off-by: GitHub <noreply@github.com>

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { connect, query } = require("./database.js");
 connect(config);
 console.log("Ready!");
 
-await query("DROP TABLE IF EXISTS dstb_user");
-await query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL UNIQUE, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
-await query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('" + config.credentials.username + "', 'Admin', 'Admin', 'test', 'Admin')");
-await query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('Test', 'Test', 'Test', 'Test', 'Test')");
+query("DROP TABLE IF EXISTS dstb_user");
+query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL UNIQUE, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
+query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('" + config.credentials.username + "', 'Admin', 'Admin', 'test', 'Admin')");
+query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('Test', 'Test', 'Test', 'Test', 'Test')");
