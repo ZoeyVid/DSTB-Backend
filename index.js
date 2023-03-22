80beb3229008cab2ca2aa772c1b0db34fe2ac740
David <david@davidcraft.de>
2023-03-22 15:44:42 +0000

    So
    Signed-off-by: GitHub <noreply@github.com>

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ const { connect, query } = require("./database.js");
 connect(config);
 console.log("Ready!");
 
-query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('Test', 'Test', 'Test', 'Test', 'Test')");
+await query("DROP TABLE IF EXISTS dstb_user");
+await query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL UNIQUE, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
+await query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('" + config.credentials.username + "', 'Admin', 'Admin', 'test', 'Admin')");
+await query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('Test', 'Test', 'Test', 'Test', 'Test')");
--- setup.js
+++ setup.js
@@ -9,7 +9,7 @@ async function createTables() {
 await database.connect(config);
 await database.query("DROP TABLE IF EXISTS dstb_user");
 // UserID, UserName, FirstName, LastName, Password-Hash, Account-Type
-await database.query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
+await database.query("CREATE TABLE IF NOT EXISTS dstb_user (id INT NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL UNIQUE, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, type VARCHAR(255), PRIMARY KEY (id))")
 // Hash Password in the future
 database.query("INSERT INTO dstb_user (username, firstname, lastname, password, type) VALUES ('" + config.credentials.username + "', 'Admin', 'Admin', 'test', 'Admin')");
 var test = await database.query("SELECT * FROM dstb_user");
